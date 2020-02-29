13aca4e7086a86478377a73090dcbdfa5fa41c8e
Stephen Fu <stephen.frx@gmail.com>
2020-03-01 00:12:11 +0800

    init project

---
+++ config/setting.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'base' => '/larastart',
+    'name' => 'Laradmin Start Template',
+    'slug' => 'LaraStart',
+    'keywords' => 'Laravel, Bootstrap, Vue.js, AdminLTE, Admin template',
+    'description' => 'An admin boilerplate developed with Bootstrap, Vue and AdminLTE base on Laravel',
+    'author' => 'Fu Rongxin',
+    'powerby' => 'Fu Rongxin',
+    'copyright' => 'LaraStart Company',
+    'password' => '123456',
+];
