e2cb0053d27d50b3105b249e216ab8f1732c26be
Stephen Fu <stephen.frx@gmail.com>
2020-03-12 23:59:05 +0800

    修复login登录逻辑

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -6,42 +6,50 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Database\QueryException;
 
 class LoginController extends Controller
 {
     public function login(Request $request)
     {
-        $request->validate([
-            'username' => 'required|string',
-            'password' => 'required|string',
-            'remember_me' => 'boolean',
-        ]);
+        try {
+            $request->validate([
+                'username' => 'required|string',
+                'password' => 'required|string',
+                'remember_me' => 'boolean',
+            ]);
 
-        $credential = $request->only('username', 'password');
+            $credential = $request->only('username', 'password');
 
-        if (!Auth::attempt($credential)) {
+            if (!Auth::attempt($credential)) {
+                return response()->json([
+                    'message' => '用户未授权',
+                    401,
+                ]);
+            }
+
+            $user = $request->user();
+            $tokenResult = $user->createToken('Personal Access Token');
+            $token = $tokenResult->token;
+
+            if ($request->remember_me) {
+                $token->expired_at = Carbon::now()->addWeek(1);
+            }
+
+            $token->save();
+
             return response()->json([
-                'message' => '用户未授权',
-                401,
+                'accessToken' => $tokenResult->accessToken,
+                'token_type' => 'Bearer',
+                'expired_at' => Carbon::parse($token->expired_at)->toDateTimeString(),
+                200,
+            ]);
+        } catch (QueryException $ex) {
+            return response()->json([
+                'message' => iconv('gbk', 'utf-8', $ex->getMessage()),
+                'code' => $ex->getCode(),
             ]);
         }
-
-        $user = $request->user();
-        $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token;
-
-        if ($request->remember_me) {
-            $token->expired_at = Carbon::now()->addWeek(1);
-        }
-
-        $token->save();
-
-        return response()->json([
-            'accessToken' => $tokenResult->accessToken,
-            'token_type' => 'Bearer',
-            'expired_at' => Carbon::parse($token->expired_at)->toDateTimeString(),
-            200,
-        ]);
     }
 
     public function logout(Request $request)
